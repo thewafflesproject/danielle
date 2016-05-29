a533da90ed77df16a1ca175ad14ac7742f44bb89
Jim Fleming <jimmy.fleming@gmail.com>
2016-05-28 20:25:52 -0700

    Update crosssiteXXXverysafe.js

--- js/crosssiteXXXverysafe.js
+++ js/crosssiteXXXverysafe.js
@@ -2,7 +2,7 @@
 (function (){
   var link = document.createElement("a");
       var img = document.createElement("img");
-      var int = Math.floor(Math.random() * (5)) ;
+      var int = Math.floor(Math.random() * (5));
       var links = [
         '//thedanielleproject.com/',
         '//thesahilproject.com/',
@@ -10,9 +10,9 @@
         '//thegabeproject.com/',
         '//themattproject.com/'
       ];
-      img.src = "//thedanielleproject.com/images/webrngs1.gif"
-      link.target = "_blank"
-      link.href = links[int]
+      img.src = "//thedanielleproject.com/images/webrngs1.gif";
+      link.target = "_blank";
+      link.href = links[int];
       link.appendChild(img);
       document.getElementById('webrng').appendChild(link);
 })();
